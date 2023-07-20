e29bc85b598464a7de63d2037c63d9ea19fb8dd5
Mayara Sebastiao <mayara.psebastiao@gmail.com>
2023-07-20 16:52:11 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+minha_string = "  qualquer texto"
+print(type(minha_string))
+print(minha_string)
+print(f"concatenar {minha_string} em string")
+print(minha_string.upper())
+print(minha_string.lower())
+print(minha_string.capitalize())
+print(minha_string.isupper())
+print(minha_string.islower())
+print(minha_string.strip())
+
